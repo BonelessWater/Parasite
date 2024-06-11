a42162e27810161a5dbf9597bbb2e269a9bfc348
lynx672 <nando.pbartolomeu@gmail.com>
2024-06-11 12:29:01 -0400

    menu screen!

---
+++ scene/menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var simultaneous_scene = preload("res://scene/game.tscn").instantiate()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	get_tree().root.add_child(simultaneous_scene)
