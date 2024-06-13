9420b35c91fed921917cad123b429414a6b5af89
lynx672 <nando.pbartolomeu@gmail.com>
2024-06-13 15:50:00 -0400

    Revert "Merge branch 'main' of https://github.com/BonelessWater/Parasite"
    This reverts commit 6259273c06e8a85557efb4ad7e175b8399a60ba6, reversing
    changes made to ddb68aa87a67416f65f87442983acf494ee34818.

--- scene/AbilityDrops/Drop.gd
+++ scene/AbilityDrops/Drop.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 # Just change these varaibles when making a new drop
-@export var ability_name := 'Dash'
+var ability_name := 'Dash'
+@export var image_name := 'crystal_01.png'
+var image := load('res://assets/items/'+image_name)
+
+var sprite
+
+func _ready():
+	sprite = get_node('Sprite2D')
+	sprite.texture = image
 
 # Give player the drop
 func _on_area_2d_body_entered(body):
 	if body.has_method('player'):
 		body.abilities[ability_name] = true
 		queue_free()
-
