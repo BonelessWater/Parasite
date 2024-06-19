df5b1a6a4c8249218ca2a2f3eebf396ce04b1f12
Sajhd000 <gabrielgarcialama@gmail.com>
2024-06-18 23:46:25 -0400

    ok

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -1,13 +1,18 @@
 extends CanvasLayer
 
 # Variable to track whether the menu is active
-var menu_active = false
+var menu_active = true
+
+var game_scene = preload("res://scene/game.tscn")
 
 func _ready():
 	# Initially hide the menu
 	hide()
 	
 	# Connect buttons to their functions
+	$MenuControl/ResumeButton.callable("pressed", self, "_on_ResumeButton_pressed")
+	$MenuControl/OptionsButton.callable("pressed", self, "_on_OptionsButton_pressed")
+	$MenuControl/ExitButton.callable("pressed", self, "_on_ExitButton_pressed")
 
 # Function to toggle the menu
 func toggle_menu():
@@ -30,3 +35,13 @@ func _on_OptionsButton_pressed():
 func _on_ExitButton_pressed():
 	# Exit to main menu or quit the game
 	get_tree().quit()
+
+func _on_resume_pressed():
+	toggle_menu()
+
+func _on_quit_pressed():
+	get_tree().quit()
+# Handle input for toggling the menu
+func _input(event):
+	if Input.is_action_just_pressed("menusito"):  # Assuming 'ui_cancel' is mapped to the key you want to use
+		toggle_menu()
