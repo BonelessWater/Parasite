3743dd791b395622cdf4ed991e92186c4b1227dd
lynx672 <153835203+lynx672@users.noreply.github.com>
2024-07-08 17:36:49 -0400

    working door! and lights!

--- door.gd
+++ door.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+var closedoor = false
+var x =1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("closed")
@@ -9,14 +10,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+		
 
 func _on_area_2d_body_entered(body):
-	if Input.is_action_just_pressed("interact"):
+	
+	
+	if body.has_method("player"):
 		$AnimatedSprite2D.play("opening")
-		
 		$Timer.start()
-
-
+		closedoor = true
+		if closedoor and x ==1:
+			$AnimatedSprite2D/Hitbox/CollisionShape2D.queue_free()
+			x=2
+			print(x)
 func _on_timer_timeout():
 	$AnimatedSprite2D.play("opened")
